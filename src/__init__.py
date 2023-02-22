baae7251508ba32aa9ca6a0f37e10feede69847f
alexbuyan <sashabuyan@yandex.ru>
2023-02-22 15:04:53 +0300

    [dev] restructuring project

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+import lark
+import ply
+
+def main():
+    print("hello from src")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+def main():
+    print("hello from tests")
+
+if __name__ == "__main__":
+    main()
