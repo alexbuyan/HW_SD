f27bd25ea7675e70540257228cc5c2719cccc3a9
diffitask <ginger.ll@mail.ru>
2023-03-01 22:56:55 +0100

    add new tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,6 +16,14 @@ class Tester:
         self.__check_result(command_str="echo something",
                             expected_result="something\n")
 
+    def test_echo_single_quotes(self):
+        self.__check_result(command_str="echo 'something in single quotes'",
+                            expected_result="something in single quotes\n")
+
+    def test_echo_double_quotes(self):
+        self.__check_result(command_str='echo "something in double quotes"',
+                            expected_result="something in double quotes\n")
+
     def test_cat_eval(self):
         print(os.getcwd())
         self.__check_result(command_str="cat tests/test_file.txt",
@@ -31,14 +39,16 @@ class Tester:
         expected_result = os.getcwd() + '\n'
         self.__check_result(command_str, expected_result)
 
-    def test_var_declaration(self):
+    def test_var_declaration_and_dollar(self):
         self.parser_manager.process_input("n = 10")
         self.__check_result(command_str="echo $n",
                             expected_result="10\n")
 
     def run_tests(self):
         self.test_echo_eval()
+        self.test_echo_single_quotes()
+        self.test_echo_double_quotes()
         self.test_cat_eval()
         self.test_wc_eval()
         self.test_pwd_eval()
-        self.test_var_declaration()
+        self.test_var_declaration_and_dollar()
